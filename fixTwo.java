a87dcca35235d335a453fb078fce8cbc16950972
John Houston <johnzinhw@gmail.com>
2019-09-23 20:15:35 -0300

    exerc2 finalizado

--- fixTwo.java
+++ fixTwo.java
@@ -4,16 +4,33 @@ import java.util.Scanner;
 public class fixTwo {
 
 	public static void main(String[] args) {
-		ArrayList<String> infoP = new ArrayList<String>();
+		int n = 3;
+		Scanner ler = new Scanner(System.in);
+		ArrayList<String> telefones = new ArrayList<String>();
 
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < n; i++) {
 			System.out.print("Nome: ");
-			String dados = new Scanner(System.in).nextLine();
-			infoP.add(dados);
+			String nome = ler.nextLine();
+
+			System.out.print("Telefone: ");
+			String telefone = ler.nextLine();
+
+			telefones.add(nome + "," + telefone);
 			System.out.println("Adicionado!");
 
-			System.out.println(dados);
 		}
 
+		System.out.print("Nome: ");
+		String nome = ler.nextLine();
+		for (int i = 0; i < n; i++) {
+			String dados_telefones = telefones.get(i);
+			System.out.println(dados_telefones);
+			String dados[] = dados_telefones.split(",");
+			if (nome == dados[0]) {
+				System.out.println(dados[1]);
+			}
+
+		}
 	}
+
 }
